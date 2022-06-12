d7cd6788071721a681d328cab195411c3877600f
Renan Aragão <refaragao@gmail.com>
2022-06-12 20:43:09 -0300

    add crete instance without default constructor

---
+++ src/BuilderImmutableObject.Test/Tire.cs
@@ -0,0 +1,8 @@
+﻿namespace BuilderImmutableObject.Test
+{
+    public class Tire
+    {
+        public int Calibration { get; set; } = 78;
+        public string Brand { get; set; } = "Pirelli";
+    }
+}
